7317025232e556f8a35cf1fda0e897ea74d8448e
SunShinewyf <2632807692@qq.com>
2017-11-04 22:51:19 +0800

    get the chrome bookmarks data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
 'use strict';
 
 require('./index.less');
-console.log('oooo')
+const $ = require('./libs/jquery.js');
+
+let bookmarksType = [];
+document.addEventListener('DOMContentLoaded',function(){
+    loadStorage();
+    initEvent();
+})
+
+//读取数据
+function loadStorage(){
+    chrome.bookmarks.getTree(function(data){
+        const result = formatTree(data[0]);
+        console.log(result);
+        renderData(result);
+    })
+
+}
+
+//初始化数据
+function initEvent(){
+
+}
+
+//将书签树数据进行遍历
+function formatTree(parent,parentTitle){
+    const that = this;
+    if(parent && parent.children){
+        if(parent.children.length){
+            parent.children.forEach(function(item){
+                let sonTitle = parentTitle
+                if(item.children && item.children.length){
+                    sonTitle = parentTitle ? parentTitle + '-' + item.title : item.title;
+                }
+                formatTree(item,sonTitle);
+            })
+        }else{
+            formatTree(null)
+        }
+    }else if(bookmarksType[parentTitle]){
+        bookmarksType[parentTitle].push(parent);
+    }else{
+        bookmarksType[parentTitle] = parent ? [parent] : that;
+    }
+    return bookmarksType;
+}
+
+//渲染书签数据
+function renderData(result){
+    const len = result.length;
+    for(let i=0; i<len; i++){
+        const html = '<div class="content" id="tags">'+
+                     '<div class="title">'+ result[i] + '</div>'+
+                     '</div>';
+                     console.log($('.wrap'));
+        $('.wrap').append(html);
+    }
+}
+
 
